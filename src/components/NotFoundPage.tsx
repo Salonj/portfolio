8f36a1fb075676a9711fe27ee047f47ae31f3db1
Salonj <124079532+Salonj@users.noreply.github.com>
2024-12-12 01:06:46 +0200

    Add i18n

---
+++ src/components/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+export function NotFoundPage() {
+    return (
+      <html lang="en">
+        <body>
+          <main>
+            <h1>OHBOY!</h1>
+          </main>
+        </body>
+      </html>
+    );
+  }
